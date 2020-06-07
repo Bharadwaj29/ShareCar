5568fb76fa560180850250b271874b56a4d588ec
BHARADWAJ CHAVA <58496849+bhanu199629@users.noreply.github.com>
2020-06-06 22:53:39 -0400

    Update connection.php

--- Car Share/connection.php
+++ Car Share/connection.php
@@ -1,6 +1,6 @@
 <?php
 //mysqli_connect("127.0.0.1", "my_user", "my_password", "my_db")
-$link = mysqli_connect("localhost", "shareca2_chava110", "Bhanu@110", "shareca2_sharecar");
+$link = mysqli_connect("localhost", "Database userName", "pswd", "shareca2_sharecar");
 if(mysqli_connect_error()){
     die('ERROR: Unable to connect:' . mysqli_connect_error()); 
     echo "<script>window.alert('Hi!')</script>";
